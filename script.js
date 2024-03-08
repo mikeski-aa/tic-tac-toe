d233a1366b77019fef2b76ffe8f05de190cae301
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-03-08 22:46:48 +0100

    Check win condition skeleton started

--- script.js
+++ script.js
@@ -66,9 +66,10 @@ const createPlayers = () => {
             p2: player('Joe', 2)
         };
 
-        return {players}
+        const gTest = () => players;
+        return {gTest}
 }
-// figure out whos turn it is to play the game
+//  figure out whos turn it is to play the game
 
 function playerTurn() {
     let tCount = 0;
@@ -77,44 +78,67 @@ function playerTurn() {
         if (tCount === 0 || tCount%2 === 0) {
             tCount += 1;
             console.log('Count is zero or even');
-            return (currTurn = players.players.p1);
+            return (currTurn = createPlayers().gTest().p1);
             
         } else {
-        console.log(`Current t score is ${tCount}`);
-        tCount += 1;
-        return (currTurn = players.players.p2);
+            console.log(`Current t score is ${tCount}`);
+            tCount += 1;
+            return (currTurn = createPlayers().gTest().p2);
         }
     }
 
     const getNextPlayer = () => next();
-
     return (getNextPlayer);
 }
 
-// finding win conditions in the board array
-// win condition is same type of marker either in one row, column or diagonally
+//  finding win conditions in the board array
+//  win condition is same type of marker either in one row, column or diagonally
 
 function checkWin(){
 
+    
     for (let i=0; i<3; i++){
-        let temp = myboard.getBoard()[i].filter((x) => x === 1);
+        let temp = myboard.getBoard()[i].filter((x) => x === createPlayers().gTest().p1.value);
         console.log(`Row ${i} - value 1 - length ${temp.length}`);
-        let temp2 = myboard.getBoard()[i].filter((x) => x === 2);
+        let temp2 = myboard.getBoard()[i].filter((x) => x === createPlayers().gTest().p2.value);
         console.log(`Row ${i} - value 2 - length ${temp2.length}`);
         
-        if ((temp.length = 3 )|| (temp2.length = 3)) {
-            console.log('we have a winner!')
+        if (temp.length = 3 && temp.filter((x) => x === 1)){
+            console.log(`${createPlayers().gTest().p1.name} is the winner`)       
+//  rest of the game needs to be notified that a winner was found
+            return;
+        } else if ((temp2.length = 3)) {
+            console.log(`${createPlayers().gTest().p2.name} is the winner`) 
+//  rest of the game needs to be notified that a winner was found
+            return;
         }
-
     }
+
+//  check diagonally if [0][0], [1][1], [2][2] are the same
+//  and if [0][2], [1][1], [2][0]
+
+    let temp = [];
+    for (let i = 0; i < 3; i++){
+        
+        temp.push(myboard.getBoard()[i][i]);
+        console.log(temp);  
+  
 }
 
-// declare stuff here
+}
 
-let players = createPlayers();
+//  declare stuff here
+
+// let players = createPlayers();
 let val = addVal();
 let myboard = gameBoard();
 let gameTurn = playerTurn();
 
-// for (x of myboard.getBoard()) { console.log(x)}
-// for (x of myboard.getBoard()[1]) { console.log([x])}
+//  for (x of myboard.getBoard()) { console.log(x)}
+//  for (x of myboard.getBoard()[1]) { console.log([x])}
+
+// myboard.markBoard(0, 0);
+// myboard.markBoard(0, 2);
+// myboard.markBoard(0, 1);
+// myboard.markBoard(2, 1);
+// myboard.markBoard(2, 1);
